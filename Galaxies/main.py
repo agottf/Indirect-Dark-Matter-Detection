32574eda9ae42d22e0b69f91bc481c8a857a71b8
Austin <agottf@gmail.com>
2021-03-01 16:40:57 -0700

    main.py now will take generic spectrums created from PhotonSpectrum.np and plot the upper limit of sigmav for one galaxy.

--- Galaxies/main.py
+++ Galaxies/main.py
@@ -12,12 +12,13 @@
 
 import numpy as np
 # import pylab as plt """It's bad practice to use pylab now"""
-# import matplotlib
+import matplotlib.pyplot as plt
 
 from scipy.interpolate import interp1d
 from scipy.integrate import quad
 from scipy.optimize import brentq
 
+import os
 
 def eflux(spectrum, emin=1e2, emax=1e5, quiet=False):
     """Integrate a generic spectrum, multiplied by E, to get the energy flux."""
@@ -33,56 +34,80 @@ def eflux(spectrum, emin=1e2, emax=1e5, quiet=False):
 # Keep numpy from complaining about dN/dE = 0...
 np.seterr(divide='ignore', invalid='ignore')
 
-# Analyze Draco with 100 GeV b-bbar spectrum.
-likefile = 'http://www-glast.stanford.edu/pub_data/1048/like_draco.txt'
-specfile = '10000'
-# J factor of Draco (*NO UNCERTAINTY INCLUDED*)
-jfactor = 10 ** 18.83
+# Define the main function that computes the limit on sigma*v given a dark matter mass
+def svlimit(specfile):
+    # Analyze Draco with some GeV b-bbar spectrum.
+    likefile = 'http://www-glast.stanford.edu/pub_data/1048/like_draco.txt'
+    # J factor of Draco (*NO UNCERTAINTY INCLUDED*)
+    jfactor = 10 ** 18.83
 
-# Spectral file created assuming J=10^18 GeV^2/cm^5, sigmav=1e-25 cm^3/s
-j0 = 1e18
-sigmav0 = 1e-25
+    # Spectral file created assuming J=10^18 GeV^2/cm^5, sigmav=1e-25 cm^3/s
+    j0 = 1e18
+    sigmav0 = 1e-25
 
-# Load the likelihood.
-data = np.loadtxt(likefile, unpack=True)
-emins, emaxs = np.unique(data[0]), np.unique(data[1])
-ebin = np.sqrt(emins * emaxs)
-efluxes = data[2].reshape(len(emins), -1)
-logLikes = data[3].reshape(len(emins), -1)
+    # Load the likelihood.
+    data = np.loadtxt(likefile, unpack=True)
+    emins, emaxs = np.unique(data[0]), np.unique(data[1])
+    ebin = np.sqrt(emins * emaxs)
+    efluxes = data[2].reshape(len(emins), -1)
+    logLikes = data[3].reshape(len(emins), -1)
 
-# Load the spectrum.
-energy, dnde = np.loadtxt(specfile, unpack=True)
-log_energy = np.log10(energy)
-log_dnde = np.log10(dnde)
-log_interp = interp1d(log_energy, log_dnde)
-spectrum = lambda e: np.nan_to_num(10 ** (log_interp(np.log10(e))))
+    # Load the spectrum.
+    energy, dnde = np.loadtxt(specfile, unpack=True)
+    log_energy = np.log10(energy)
+    log_dnde = np.log10(dnde)
+    log_interp = interp1d(log_energy, log_dnde)
+    spectrum = lambda e: np.nan_to_num(10 ** (log_interp(np.log10(e))))
 
-# Predict energy flux from nominal spectral values
-pred = np.array([eflux(spectrum, e1, e2) for e1, e2 in zip(emins, emaxs)])
+    # Predict energy flux from nominal spectral values
+    pred = np.array([eflux(spectrum, e1, e2) for e1, e2 in zip(emins, emaxs)])
 
-# Interpolated log-likelihood in each energy bin
-likes = [interp1d(f, l - l.max()) for f, l in zip(efluxes, logLikes)]
+    # Interpolated log-likelihood in each energy bin
+    likes = [interp1d(f, l - l.max()) for f, l in zip(efluxes, logLikes)]
 
-# Global log-likelihood summed over energy bins
-like = lambda c: sum([lnlfn(c * p) for lnlfn, p in zip(likes, pred)])
+    # Global log-likelihood summed over energy bins
+    like = lambda c: sum([lnlfn(c * p) for lnlfn, p in zip(likes, pred)])
 
-# Scan range for global log-likelihood
-epsilon = 1e-4  # Just to make sure we stay within the interpolation range
-xmin = epsilon
-xmax = np.log10(efluxes.max() / efluxes[efluxes > 0].min()) - epsilon
-x = np.logspace(xmin, xmax, 250)
+    # Scan range for global log-likelihood
+    epsilon = 1e-4  # Just to make sure we stay within the interpolation range
+    xmin = epsilon
+    xmax = np.log10(efluxes.max() / efluxes[efluxes > 0].min()) - epsilon
+    x = np.logspace(xmin, xmax, 250)
 
-norm0 = efluxes[efluxes > 0].min() / pred.max()
-norms = norm0 * x
+    norm0 = efluxes[efluxes > 0].min() / pred.max()
+    norms = norm0 * x
 
-# Global log-likelihood using nominal value of nuisance parameter
-lnl = np.array([like(n) for n in norms])
+    # Global log-likelihood using nominal value of nuisance parameter
+    lnl = np.array([like(n) for n in norms])
 
-# Convert global log-likelihood back into physical units
-sigmav = j0 / jfactor * sigmav0 * norms
-lnlfn = interp1d(sigmav, lnl)
-mle = lnl.max()
-sigmav_mle = sigmav[lnl.argmax()]
-delta = 2.71 / 2
-limit = brentq(lambda x: lnlfn(x) - mle + delta, sigmav_mle, sigmav.max(), xtol=1e-10 * sigmav_mle)
-print("Upper Limit: %.5e" % limit)
+    # Convert global log-likelihood back into physical units
+    sigmav = j0 / jfactor * sigmav0 * norms
+    lnlfn = interp1d(sigmav, lnl)
+    mle = lnl.max()
+    sigmav_mle = sigmav[lnl.argmax()]
+    delta = 2.71 / 2
+    limit = brentq(lambda x: lnlfn(x) - mle + delta, sigmav_mle, sigmav.max(), xtol=1e-10 * sigmav_mle)
+    return(limit)
+
+svlimit("100.txt")
+# Define list of sigma*v vs dark matter mass
+svlist = np.array([])
+mdmlist = np.array([])
+
+directory = os.fsencode("/Users/austingottfredson/Indirect-Dark-Matter-Detection/Galaxies/")
+
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    if filename.endswith(".txt"):
+        svlist = np.append(svlist, [svlimit(filename)], axis=0)
+        mdmlist = np.append(mdmlist, float(filename.removesuffix(".txt")))
+        continue
+    else:
+        continue
+# print(svlist)
+# print(mdmlist)
+
+plt.loglog(mdmlist, svlist,'ro')
+plt.xlabel("DM Mass (GeV/c^2)")
+plt.ylabel("sigma*v (cm^3 s^-1)")
+plt.show()
