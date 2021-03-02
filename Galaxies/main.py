64ada9da1851cc14930258b671d941c818acd20f
Austin <agottf@gmail.com>
2021-03-02 16:06:50 -0700

    I added a list of galaxy likelihood files and their j factor. Working on adding likelihoods together.

--- Galaxies/main.py
+++ Galaxies/main.py
@@ -20,6 +20,7 @@ from scipy.optimize import brentq
 
 import os
 
+
 def eflux(spectrum, emin=1e2, emax=1e5, quiet=False):
     """Integrate a generic spectrum, multiplied by E, to get the energy flux."""
     espectrum = lambda e: spectrum(e) * e
@@ -34,18 +35,19 @@ def eflux(spectrum, emin=1e2, emax=1e5, quiet=False):
 # Keep numpy from complaining about dN/dE = 0...
 np.seterr(divide='ignore', invalid='ignore')
 
+
 # Define the main function that computes the limit on sigma*v given a dark matter mass
 def svlimit(specfile):
     # Analyze Draco with some GeV b-bbar spectrum.
-    likefile = 'http://www-glast.stanford.edu/pub_data/1048/like_draco.txt'
+    likefile = galaxies[0,0]
     # J factor of Draco (*NO UNCERTAINTY INCLUDED*)
-    jfactor = 10 ** 18.83
+    jfactor = 10 ** float(galaxies[0,1])
 
     # Spectral file created assuming J=10^18 GeV^2/cm^5, sigmav=1e-25 cm^3/s
     j0 = 1e18
     sigmav0 = 1e-25
 
-    # Load the likelihood.
+    # Load the likelihood. Then add the log likelihood together
     data = np.loadtxt(likefile, unpack=True)
     emins, emaxs = np.unique(data[0]), np.unique(data[1])
     ebin = np.sqrt(emins * emaxs)
@@ -87,15 +89,34 @@ def svlimit(specfile):
     sigmav_mle = sigmav[lnl.argmax()]
     delta = 2.71 / 2
     limit = brentq(lambda x: lnlfn(x) - mle + delta, sigmav_mle, sigmav.max(), xtol=1e-10 * sigmav_mle)
-    return(limit)
+    return (limit)
 
-svlimit("100.txt")
+
 # Define list of sigma*v vs dark matter mass
 svlist = np.array([])
 mdmlist = np.array([])
 
+# Make a list of likelihood files for each galaxy with its associated J-factor and uncertainty
+galaxies = np.array([("http://www-glast.stanford.edu/pub_data/1048/like_bootes_I.txt", 18.8, .22),
+                     ("http://www-glast.stanford.edu/pub_data/1048/like_canes_venatici_II.txt", 17.9, .25),
+                     ("http://www-glast.stanford.edu/pub_data/1048/like_carina.txt", 18.1, .23),
+                     ("http://www-glast.stanford.edu/pub_data/1048/like_coma_berenices.txt", 19.0, .25),
+                     ("http://www-glast.stanford.edu/pub_data/1048/like_draco.txt", 18.8, .16),
+                     ("http://www-glast.stanford.edu/pub_data/1048/like_fornax.txt", 18.2, .21),
+                     ("http://www-glast.stanford.edu/pub_data/1048/like_hercules.txt", 18.1, .23),
+                     ("http://www-glast.stanford.edu/pub_data/1048/like_leo_II.txt", 17.6, .18),
+                     ("http://www-glast.stanford.edu/pub_data/1048/like_leo_IV.txt", 17.9, .28),
+                     ("http://www-glast.stanford.edu/pub_data/1048/like_sculptor.txt", 18.6, .18),
+                     ("http://www-glast.stanford.edu/pub_data/1048/like_segue_1.txt", 19.5, .29),
+                     ("http://www-glast.stanford.edu/pub_data/1048/like_sextans.txt", 18.4, .27),
+                     ("http://www-glast.stanford.edu/pub_data/1048/like_ursa_major_II.txt", 19.3, .28),
+                     ("http://www-glast.stanford.edu/pub_data/1048/like_ursa_minor.txt", 18.8, .19),
+                     ("http://www-glast.stanford.edu/pub_data/1048/like_willman_1.txt", 19.1, .31)])
+
+# Where the spectrum files are located
 directory = os.fsencode("/Users/austingottfredson/Indirect-Dark-Matter-Detection/Galaxies/")
 
+# Make the list of sigma*v and dark matter mass
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
     if filename.endswith(".txt"):
@@ -107,7 +128,7 @@ for file in os.listdir(directory):
 # print(svlist)
 # print(mdmlist)
 
-plt.loglog(mdmlist, svlist,'ro')
+plt.loglog(mdmlist, svlist, 'ro')
 plt.xlabel("DM Mass (GeV/c^2)")
 plt.ylabel("sigma*v (cm^3 s^-1)")
 plt.show()
